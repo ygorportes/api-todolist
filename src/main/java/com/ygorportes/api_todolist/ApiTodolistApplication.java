098f604fb38de55f92d14a8cd3616addc9ddacd9
Ygor Portes <ygorportes3@gmail.com>
2024-06-19 12:06:22 -0300

    feat: first commit

---
+++ src/main/java/com/ygorportes/api_todolist/ApiTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.ygorportes.api_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ygorportes/api_todolist/ApiTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygorportes.api_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
